34429932a2569e4ea8c083ece1be73f268b05ae3
Volodymyr Tkachov <w68179@student.wsiz.edu.pl>
2023-10-19 16:01:16 +0200

    lab2

---
+++ Constructions/Construction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Constructions
+{
+    class Construction
+    {
+        public Construction(double height, double width, int entrances,
+            int humanCapacity, string buildMaterial)
+        {
+            Height = height;
+            Width = width;
+            Entrances = entrances;
+            HumanCapacity = humanCapacity;
+            BuildMaterial = buildMaterial; 
+        }
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public int Entrances { get; set; }
+        public int HumanCapacity { get; set; }
+        public string BuildMaterial { get; set; }
+    }       
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using ConsoleApp1.Constructions;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var construction = new Construction(23, 45, 4, 6, "Brik");
+            Console.WriteLine(construction.BuildMaterial);
+        }
+    }
+}
